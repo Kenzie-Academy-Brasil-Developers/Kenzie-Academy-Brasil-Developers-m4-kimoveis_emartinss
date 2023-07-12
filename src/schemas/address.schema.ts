027540c97fb7c8af5d192f8cef944f27f1280ec9
emartinss <edumartinscmp@gmail.com>
2023-07-12 19:18:17 -0300

    add min zod

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export const AddressSchema = z.object({
   id: z.number().optional(),
-  street: z.string().max(45).min(1),
-  zipCode: z.string().max(8).min(1),
-  number: z.string().max(7).min(1).nullish(),
-  city: z.string().max(20).min(1),
-  state: z.string().max(2).min(1),
+  street: z.string().max(45),
+  zipCode: z.string().max(8),
+  number: z.string().max(7).nullish(),
+  city: z.string().max(20),
+  state: z.string().max(2),
 });
 
 export const addressCreate = AddressSchema.omit({id:true})
